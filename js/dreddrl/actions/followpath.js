ae0475822ab28735d15af5eb5634213fc3feaf7b
Marshall Petersen <marshall.petersen@gmail.com>
2013-05-29 18:12:41 -0700

    Added docs, and missing component.

---
+++ js/dreddrl/actions/followpath.js
@@ -0,0 +1,34 @@
+define(['sge','../action'], function(sge, Action){
+	var FollowPathAction = Action.extend({
+		start: function(){
+	        this.async = true;
+	        var tx = this.entity.get('xform.tx');
+	        var ty = this.entity.get('xform.ty');
+	        var tileX = Math.floor(tx/32)
+	        var tileY = Math.floor(ty/32)
+	        this.pathPoints = this.state.map.getPath(tileX, tileY,3,3);
+		},
+		tick: function(delta){
+			console.log(this.pathPoints.length)
+			if (this.pathPoints.length<=0){
+				this.entity.set('xform.v', 0, 0);
+        		this.end();
+        		return;
+        	}
+			var speed = 64;
+			var tx = this.entity.get('xform.tx');
+	        var ty = this.entity.get('xform.ty');
+	        var goalX = this.pathPoints[0][0];
+	        var goalY = this.pathPoints[0][1];
+	        var dx = goalX - tx;
+	        var dy = goalY - ty;
+	        var dist = Math.sqrt((dx*dx)+(dy*dy));
+	        if (dist<6){
+	        	this.pathPoints.shift();
+	        }
+	        this.entity.set('xform.v', dx / dist * speed, dy / dist * speed);
+		}
+	});
+	Action.register('followpath', FollowPathAction);
+	return FollowPathAction
+})
