6ddd9c5ff2ccf94a740ef42b7e47899dffdc77b8
Marshall Petersen <marshall.petersen@gmail.com>
2013-09-09 19:16:43 -0700

    Updated scaling for better fps.

--- assets/game.js
+++ assets/game.js
@@ -15,18 +15,32 @@ function   (sge, dreddrl) {
     //Setup Ratio
     //var body = $('body');
     var idealWidth = parseInt(getURLParameter('width') || 640);
-    var idealHeight = parseInt(getURLParameter('height') || 480);
+    var idealHeight = parseInt(getURLParameter('height') || 360);
     var idealFPS = parseInt(getURLParameter('fps') || 60);
     canvasElem = document.getElementById('game');
     
     //Full Screen Always
-    idealWidth = window.innerWidth;
-    idealHeight = window.innerHeight;
-    canvasElem.className = "fullscreen";
-
-    var idealRatio = idealWidth/idealHeight;
-    canvasElem.style.width = idealWidth + 'px';
-    canvasElem.style.height = idealHeight + 'px';
+    
+    var resizeCallback = function(){
+        var innerWidth = window.innerWidth;
+        var innerHeight = window.innerHeight;
+        var pixelWidth, pixelHeight;
+        if ((innerWidth / innerHeight) < 1.7777){
+            pixelWidth = innerWidth;
+            pixelHeight = Math.round(innerWidth / 1.77777);
+            console.log('Tall', pixelWidth, pixelHeight);
+        } else {
+            pixelHeight = innerHeight;
+            pixelWidth = Math.round(innerHeight * 1.7777);
+            console.log('Wide', pixelWidth, pixelHeight);
+        }
+        canvasElem.style.width = pixelWidth + 'px';
+        canvasElem.style.height = pixelHeight + 'px';
+        canvasElem.style['margin-left'] = (innerWidth - pixelWidth)/2 + 'px';
+        canvasElem.style['margin-top'] = (innerHeight - pixelHeight)/2 + 'px';
+    }
+    window.onresize = resizeCallback;
+    resizeCallback();
     CAAT.DEBUG=Boolean(getURLParameter('caat-debug'));
     game = new sge.Game({elem: document.getElementById('game'), pauseState: dreddrl.PauseState, mainMenuState: dreddrl.MainMenuState, width: idealWidth, height: idealHeight, fps:idealFPS});
     var state = game.setGameState(dreddrl.DreddRLState);
