d8a87f4b57201938a81f354f918b351e308a5817
Marshall Petersen <marshall.petersen@gmail.com>
2013-10-24 23:32:21 -0700

    Updated gfx.

---
+++ content/quests/hideout.js
@@ -0,0 +1,10 @@
+var room = block.getRandomRoom();
+room.lockDoors();
+room.highlight(true);
+for (var i = 18 - 1; i >= 0; i--) {
+	room.spawn('lawbreaker', {
+		ai: {
+			region: room
+		}
+	});
+};
